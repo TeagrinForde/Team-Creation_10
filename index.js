54d46d9487a0b5f98bdd8e34d4ee5839ad97502a
Teagrin Forde <teagrinb@gmail.com>
2022-07-14 17:20:50 -0400

    index.js linked to classes.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//pulls collected information
+const EmployeeForm = require('./lib/classes');
+
+//puts the collected information into a new employee form
+const employeeForm = new EmployeeForm();
+
+//starts the employee form
+employeeForm.start();
